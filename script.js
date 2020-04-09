84808d8345821553f536893d11df4e7f9d28771e
Leirach <juandleon27@gmail.com>
2020-04-09 10:48:39 -0500

    added dogs and breed search

--- script.js
+++ script.js
@@ -4,12 +4,39 @@ new Vue({
         title: "Dog API Client",
         subtitle: "By Juan Carlos De León",
         imgURL: "",
+        btnText: "Another Dog!",
+        loading: false,
+        navString: "Collapse",
+        jumbotronVisible: true,
+        breed: "",
+        showError: false,
     },
     methods: {
         changeImg() {
-            axios.get('https://dog.ceo/api/breeds/image/random')
-                 .then(response => (this.imgURL = response.data.message))
+            this.showError = false;
+            let endpoint = 'https://dog.ceo/api/breeds/image/random'
+            if(this.breed){
+                endpoint = `https://dog.ceo/api/breed/${this.breed}/images/random`
+            }
+            this.loading = true;
+            this.btnText = "loading..."
+            axios.get(endpoint)
+                .then(response => {
+                    this.imgURL = response.data.message;
+                })
+                .catch((err) => {
+                    this.imgLoaded();
+                    this.showError = true
+                });
         },
+        toggleJumbotron() {
+            this.jumbotronVisible = !this.jumbotronVisible;
+            this.navString = this.jumbotronVisible? "Collapse" : "Show";
+        },
+        imgLoaded() {
+            this.loading = false;
+            this.btnText = "Another Dog!";
+        }
     },
     mounted(){
         this.changeImg();
